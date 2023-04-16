1307f2264dc2a4edb7eb1f3bc8fc4e74e2e3be67
jlorenzo <jair.lorenzo.l@outlook.com>
2023-04-16 04:16:43 -0500

    Add weblogic configuration

--- src/main/java/pe/com/indigitalxp/customerserviceapi/CustomerServiceApiApplication.java
+++ src/main/java/pe/com/indigitalxp/customerserviceapi/CustomerServiceApiApplication.java
@@ -2,12 +2,20 @@ package pe.com.indigitalxp.customerserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class CustomerServiceApiApplication {
+public class CustomerServiceApiApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CustomerServiceApiApplication.class);
 	}
 
 }
